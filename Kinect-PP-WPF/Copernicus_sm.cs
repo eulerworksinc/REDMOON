b5ffdf6c6f0d0f5710285bb3058719051b3e15ed
Edward Sayers <hirangren@yahoo.com>
2015-11-08 16:18:10 -0800

    Start xml parser

---
+++ Kinect-PP-WPF/Copernicus_sm.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+using Microsoft.Office.Interop.PowerPoint;
+
+namespace Kinect_PP_WPF
+{
+    class Copernicus_sm
+    {
+        int current_slide;
+
+
+        public Copernicus_sm(string xml_filename)
+        {
+            
+        }
+
+        
+
+
+    }
+
+    class Copernicus_slide
+    {
+        public bool next_slide_button;
+        public bool prev_slide_button;
+        public bool close_button;
+        public bool left_button;
+        public bool right_button;
+    }
+}
